9dd6e4db50cfcfca8c486b20179a9125e61109dd
Åge Sølvberg <soelvberg@gmail.com>
2025-12-02 10:21:11 +0100

    Day 2 cleanup

--- 02/main.go
+++ 02/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"math"
 	"os"
 	"strings"
 	"time"
@@ -26,30 +25,26 @@ func main() {
 
 	file, err := os.Open("input.txt")
 	if err != nil {
-		log.Fatalf("Failed to open file: %v", err)
+		log.Fatalf("Error open file: %v", err)
 	}
 	defer file.Close()
 
 	ranges := parseFile(file)
 
 	for _, r := range ranges {
-		//fmt.Printf("Range: %d - %d\n", r.Start, r.End)
-
 		for i := r.Start; i <= r.End; i++ {
 			if checkRepeatePattern(i) {
 				sum = sum + i
 			}
 		}
 	}
+
 	fmt.Printf("Sum: %d\n", sum)
 }
 
 func checkRepeatePattern(num int) bool {
 	productIdStr := fmt.Sprintf("%d", num)
 	productIdLength := len(productIdStr)
-	// if productIdLength%2 != 0 {
-	// 	return false
-	// }
 
 	for i := 1; i < productIdLength; i++ {
 		if productIdLength%i != 0 {
@@ -57,13 +52,6 @@ func checkRepeatePattern(num int) bool {
 		}
 
 		partCount := productIdLength / i
-		// test := productIdLength / i
-		// if !IsInt(float64(test)) {
-		// 	continue
-		// }
-		// partCount := test
-
-		// fmt.Printf("Checking num: %d with part length %d (part count %d)\n", num, i, partCount)
 
 		parts := make([]string, partCount)
 		for j := 0; j < partCount; j++ {
@@ -82,13 +70,6 @@ func checkRepeatePattern(num int) bool {
 
 	}
 	return false
-
-	// half := n / 2
-	// return s[:half] == s[half:]
-}
-
-func IsInt(f float64) bool {
-	return math.Mod(f, 1) == 0
 }
 
 func parseFile(file *os.File) []Range {
@@ -99,8 +80,8 @@ func parseFile(file *os.File) []Range {
 		log.Fatalf("Error read file: %v", err)
 	}
 	data := string(content[:n])
-	parts := strings.Split(data, ",")
-	for _, part := range parts {
+	parts := strings.SplitSeq(data, ",")
+	for part := range parts {
 		var r Range
 		_, err := fmt.Sscanf(part, "%d-%d", &r.Start, &r.End)
 		if err != nil {
