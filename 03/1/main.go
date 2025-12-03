0f7ad76d293a711fa1e10898596473536ffc3f75
Åge Sølvberg <soelvberg@gmail.com>
2025-12-03 10:49:27 +0100

    Day 3

--- 03/1/main.go
+++ 03/1/main.go
@@ -1,9 +1,80 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	startTime := time.Now()
+	defer func() {
+		elapsed := time.Since(startTime)
+		log.Printf("Execution time: %s", elapsed)
+	}()
+
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var sum int = 0
+	// var lines [][]int
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		digits := make([]int, 0, len(line))
+
+		for _, ch := range line {
+			digit, err := strconv.Atoi(string(ch))
+			if err != nil {
+				continue
+			}
+			digits = append(digits, digit)
+		}
+
+		// lines = append(lines, digits)
+
+		joltage := findLargestJoltage(digits)
+		fmt.Println("Largest joltage:", joltage)
+
+		sum += joltage
+	}
+
+	fmt.Println("Sum:", sum)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func findLargestJoltage(digits []int) int {
+	first := 0
+	second := 0
+	indexFirst := -1
+
+	for i, num := range digits[:len(digits)-1] {
+		if num > first {
+			first = num
+			indexFirst = i
+		}
+	}
+
+	for i := indexFirst + 1; i < len(digits); i++ {
+		if digits[i] > second {
+			second = digits[i]
+		}
+	}
+
+	resultStr := fmt.Sprintf("%d%d", first, second)
+	result, err := strconv.Atoi(resultStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return result
 }
--- 03/2/main.go
+++ 03/2/main.go
@@ -1,9 +1,93 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
 )
 
+const batteryCount = 12
+
 func main() {
-	fmt.Println("Hello, World!")
+	startTime := time.Now()
+	defer func() {
+		elapsed := time.Since(startTime)
+		log.Printf("Execution time: %s", elapsed)
+	}()
+
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var sum int = 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		bank := make([]int, 0, len(line))
+
+		for _, ch := range line {
+			digit, err := strconv.Atoi(string(ch))
+			if err != nil {
+				continue
+			}
+			bank = append(bank, digit)
+		}
+
+		joltage := findLargestJoltage(batteryCount, bank)
+
+		sum += joltage
+	}
+
+	fmt.Println("Sum:", sum)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func findLargestJoltage(count int, digits []int) int {
+	numDigits := count
+
+	selectedDigits := []int{}
+
+	lastIndex := -1
+
+	var high func(n int)
+	high = func(n int) {
+
+		if n == 0 {
+			return
+		}
+
+		highestDigit := -1
+
+		for i := lastIndex + 1; i < len(digits)-(n-1); i++ {
+			if digits[i] > highestDigit {
+				highestDigit = digits[i]
+				lastIndex = i
+			}
+		}
+		selectedDigits = append(selectedDigits, highestDigit)
+
+		high(n - 1)
+	}
+
+	high(numDigits)
+
+	resultStr := ""
+	for _, digit := range selectedDigits {
+		resultStr += strconv.Itoa(digit)
+	}
+
+	result, err := strconv.Atoi(resultStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return result
 }
